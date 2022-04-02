6e468483be2a8c7d577045f3eb0ab94b902aaf0b
hanadup <92228126+hanadup@users.noreply.github.com>
2022-04-02 16:14:39 +0100

    Update Week 4 HTL.py

--- Week 4 HTL.py
+++ Week 4 HTL.py
@@ -4,3 +4,20 @@ new_file.write("3" + "\n" + "45" + "\n" + "83" + "\n" + "21" + "\n")
 new_file.close()
 
 #Task 2
+score = int(input("What was the score of the test you have taken? "))
+
+def mark_grade():
+    if 20 <= score <= 29:
+        print ("Your grade is D")
+    elif 30 <= score <= 49:
+        print ("Your grade is C")
+    elif 50 <= score <= 69:
+        print("Your grade is B")
+    elif 70 <= score <= 100:
+        print("Your grade is A")
+    elif score < 20:
+        print(" Your grade is F")
+    else:
+        print("Please try again score inputted was incorrect.")
+
+mark_grade()
